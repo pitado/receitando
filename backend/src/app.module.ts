39f1c409b8e4829491a9d5e0ae5d548961de30ba
Miguel Pita <Miguellpitaa@gmail.com>
2026-08-17 16:45:39 -0300

    feat(auth): enable authentication routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './modules/auth/auth.module';
 import { HealthModule } from './modules/health/health.module';
 import { IngredientsModule } from './modules/ingredients/ingredients.module';
 import { RecipesModule } from './modules/recipes/recipes.module';
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    AuthModule,
     HealthModule,
     IngredientsModule,
     RecipesModule,
