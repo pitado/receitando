550108ee95e4fd4342122ee548601ede6c509047
Miguel Pita <Miguellpitaa@gmail.com>
2026-08-17 17:38:17 -0300

    feat(auth): connect registration page to API

--- frontend/src/app/cadastro/page.tsx
+++ frontend/src/app/cadastro/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 
+import { RegisterForm } from "./RegisterForm";
 import styles from "../entrar/page.module.css";
 
 export const metadata: Metadata = {
@@ -33,24 +34,7 @@ export default function RegisterPage() {
             <p>Leva menos de um minuto.</p>
           </div>
 
-          <form className={styles.form}>
-            <label className={styles.field}>
-              <span>Nome</span>
-              <input autoComplete="name" name="name" placeholder="Seu nome" required type="text" />
-            </label>
-
-            <label className={styles.field}>
-              <span>E-mail</span>
-              <input autoComplete="email" inputMode="email" name="email" placeholder="voce@exemplo.com" required type="email" />
-            </label>
-
-            <label className={styles.field}>
-              <span>Senha</span>
-              <input autoComplete="new-password" minLength={10} name="password" placeholder="Pelo menos 10 caracteres" required type="password" />
-            </label>
-
-            <button className={styles.submit} type="submit">Criar conta</button>
-          </form>
+          <RegisterForm />
 
           <div className={styles.divider}><span>ou</span></div>
 
