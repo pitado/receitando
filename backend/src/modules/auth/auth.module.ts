b30bba30111b6aeb4392bf537ef0d5134f3cc436
Miguel Pita <Miguellpitaa@gmail.com>
2026-08-17 16:45:28 -0300

    feat(auth): wire authentication module

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PasswordService } from './password.service';
+import { SessionService } from './session.service';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, PasswordService, SessionService],
+  exports: [AuthService],
+})
+export class AuthModule {}
