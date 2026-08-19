53f151b4d3e335ce856af9491e6cfd372b63f802
Miguel Pita <Miguellpitaa@gmail.com>
2026-08-19 09:29:38 -0300

    chore: move metadados de imagem para PR dedicado

--- backend/worker-prototype/migrations/0011_recipe_image_metadata.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-ALTER TABLE recipes ADD COLUMN image_source TEXT;
-ALTER TABLE recipes ADD COLUMN image_author TEXT;
-ALTER TABLE recipes ADD COLUMN image_source_url TEXT;
-
-CREATE INDEX IF NOT EXISTS idx_recipes_image_source ON recipes(image_source);
