bab73acf3e1d0d60a8890d445f1805992c8a5307
Miguel Pita <Miguellpitaa@gmail.com>
2026-09-02 15:15:27 -0300

    refactor: remove logo do header e centraliza navegação

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,18 +1,13 @@
 import { AuthControls } from "./AuthControls";
 import { HeaderNav } from "./HeaderNav";
-import { InteractiveBrand } from "./InteractiveBrand";
 import styles from "./Header.module.css";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.inner}`}>
-        <div className={styles.brandSlot}>
-          <InteractiveBrand />
-        </div>
-
+        <span aria-hidden="true" className={styles.balance} />
         <HeaderNav />
-
         <AuthControls />
       </div>
     </header>
